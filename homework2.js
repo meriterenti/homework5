fe9819a2d1a21356b8ba8aff8389a8b15c53a29f
meriterenti <armen_aslikian@mail.ru>
2018-09-13 21:57:26 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,32 @@
+const studentName = 'Meri'
+
+const homeWork = {
+	videoPoints: [
+		{video: 35},
+		{video: 30}
+	],
+	programming: [
+		{
+			createHtml: 5,
+			constants: 5,
+			arithmetics: 5,
+			numAndString: 5,
+			ifElseBlock: 10,
+			thingToTheConsole: 5
+		}
+	]
+}
+
+const passingResult = homeWork.videoPoints[0].video + homeWork.programming[0].createHtml;
+const bestResult = homeWork.videoPoints[0].video + homeWork.videoPoints[1].video + homeWork.programming[0].createHtml*6+5;
+const studentResult = 45;
+
+if(studentResult === bestResult){
+	console.log('Yaay '+studentName+', You are the best');
+} else if(studentResult < bestResult && studentResult >=passingResult){
+	console.log('Congratulatins '+studentName+'! You passed');
+} else{
+	console.log("I just don't know what to say");
+}
+
+console.log('Thank you for your time !');
